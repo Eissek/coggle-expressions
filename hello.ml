97d8b0db4fa6c28bf95b8f2463ff49300651d172
Eissek <sane.gez@gmail.com>
2016-05-30 23:34:09 +0100

    connection is closed after its handled

--- hello.ml
+++ hello.ml
@@ -8,7 +8,7 @@ let handler ~body:_ _sock req =
   match Uri.path uri with
   | "/test" ->
     Uri.get_query_param uri "hello"
-      |> Option.map ~f:(fun v -> "hello: " ^ v)
+      |> Option.map ~f:(fun v -> "hello: " ^ v ^ "\n")
       |> Option.value ~default:"No param hello supplied"
       |> Server.respond_with_string
   | _ ->
@@ -18,9 +18,18 @@ let handler ~body:_ _sock req =
 let start_server port () =
   eprintf "Listening for HTTP on port %d\n" port;
   eprintf "Try 'curl http://localhost:%d/test?hello=xyz'\n%!" port;
-  Cohttp_async.Server.create ~on_handler_error:`Raise
-    (Tcp.on_port port) handler
-    >>= fun _ -> Deferred.never ()
+  (* let server = *)
+    Cohttp_async.Server.create ~on_handler_error:`Raise
+      (Tcp.on_port port) handler
+      >>= fun addr -> Cohttp_async.Server.close addr
+    (* >>= fun _ -> Deferred.never () *)
+      (* Cohttp_async.Server.c *)
+
+
+  (* ignore (server : (Socket.Address.Inet.t, int) Cohttp_async.Server.t Deferred.t) *)
+
+(* Cohttp_async.Server.c *)
+    (* Deferred.never () *)
 
 let () =
   Command.async
