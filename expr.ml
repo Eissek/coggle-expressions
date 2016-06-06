f23ff9e3aeee0c6c1395edbd61bb44a572d25368
Eissek <sane.gez@gmail.com>
2016-06-06 16:36:31 +0100

    start expr

--- expr.ml
+++ expr.ml
@@ -1,9 +1,51 @@
 open Core.Std
-open Cohttp
-open Lwt
-open Cohttp_lwt
+(* open Cohttp *)
+(* open Lwt *)
+(* open Cohttp_lwt *)
 open Async.Std
+open Cohttp_async
 (* open Cohttp_lwt_unix *)
 
+
+let extract req =
+  let uri = Cohttp.Request.uri req in
+  match Uri.get_query_param uri "hello" with
+  | Some(hello) -> if hello = "" then  None else Some hello
+  | None -> None
+
+
+let start_server port () =
+  eprintf "Listening for HTTP on port %d\n" port;
+  eprintf "Try 'curl http://localhost:%d/test?hello=xyz'\n%!" port;
+  let param = ref false in
+  let inet_addr = ref None in
+  Cohttp_async.Server.create ~on_handler_error:`Raise
+    (Tcp.on_port port)
+    (fun ~body: _ _sock req ->
+       let uri = Cohttp.Request.uri req in
+       match Uri.path uri with
+       | "/test" ->
+         extract req
+           |> (fun _ ->
+             match !inet_addr with
+             | None -> Server.respond_with_string "inet address not found"
+             | Some y -> Cohttp_async.Server.close y
+                         |> fun _ -> Server.respond_with_string "bye\n")
+       | _ -> Server.respond_with_string ~code:`Not_found "Route not found\n"
+    )
+  >>= fun addr -> let set_inet =
+                    inet_addr := Some addr
+  in Deferred.never
+    set_inet
+
+
+let () =
+  Command.async
+    ~summary:"Start a hello world Async server"
+    Command.Spec.(empty +>
+                  flag "-p" (optional_with_default 8080 int)
+                    ~doc:"int Source port to listen on"
+                 ) start_server
+  |> Command.run
 
 
